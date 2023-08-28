45f32858d77f9a21abba52bad89a7cae1da6a30a
Jonas <Jonas@192.168.0.40>
2023-08-28 20:54:55 +0200

    Added dalle-api

--- src/gen/Main.java
+++ src/gen/Main.java
@@ -3,8 +3,7 @@ package gen;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import gen.set.SetBuilder;
-import gen.set.SetImageGenerator;
+import gen.imageGenerator.ImageGenerator;
 import gen.set.SetLoader;
 import gen.set.definitions.Set;
 
@@ -12,9 +11,12 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		Path currentPath = Paths.get(System.getProperty("user.dir"));
 		Set base = SetLoader.loadSet("base");
-		SetImageGenerator generator = new SetImageGenerator(base, currentPath.resolve("img"));
-		generator.generateImages();
-		SetBuilder builder = new SetBuilder(base, currentPath.resolve("build"), currentPath.resolve("img"));
-		builder.buildFaces();
+//		SetImageGenerator generator = new SetImageGenerator(base, currentPath.resolve("img"));
+//		generator.generateImages();
+//		SetBuilder builder = new SetBuilder(base, currentPath.resolve("build"), currentPath.resolve("img"));
+//		builder.buildFaces();
+		
+		ImageGenerator gen = new ImageGenerator();
+		gen.generateImage("a factory, its belts run in mutliple dimensions, yellow, sci-fi, digital painting, realistc", currentPath);
 	}
 }
---
+++ src/gen/imageGenerator/ImageGenerator.java
@@ -0,0 +1,126 @@
+package gen.imageGenerator;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.time.Instant;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.hc.client5.http.async.methods.SimpleHttpResponse;
+import org.apache.hc.client5.http.async.methods.SimpleResponseConsumer;
+import org.apache.hc.client5.http.impl.async.CloseableHttpAsyncClient;
+import org.apache.hc.client5.http.impl.async.HttpAsyncClients;
+import org.apache.hc.core5.concurrent.FutureCallback;
+import org.apache.hc.core5.http.HttpHost;
+import org.apache.hc.core5.http.nio.AsyncRequestProducer;
+import org.apache.hc.core5.http.nio.AsyncResponseConsumer;
+import org.apache.hc.core5.http.nio.support.AsyncRequestBuilder;
+import org.apache.hc.core5.reactor.IOReactorConfig;
+import org.apache.hc.core5.util.Timeout;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class ImageGenerator {
+
+	//https://ptsv3.com/t/asdf/
+	//https://httpbin.org/post 
+//	private final static String API_STRING_HOST = "httpbin.org";
+//	private final static String API_STRING_URI = "/post";
+	private final static String API_STRING_HOST = "api.openai.com";
+	private final static String API_STRING_URI = "/v1/images/generations";
+	private final static String API_KEY = "";
+
+	public void generateImage(String prompt, Path targetFolderPath) throws ImageGeneratorException {
+		try {
+			callApi(prompt, targetFolderPath);
+		} catch (Exception e) {
+			throw new ImageGeneratorException("Failed image generation", e);
+		}
+	}
+
+	private void callApi(String prompt, Path targetFolderPath) throws Exception {
+
+		Integer n = 4;
+		IOReactorConfig ioReactorConfig = IOReactorConfig.custom().setSoTimeout(Timeout.ofSeconds(n * 60)).build();
+
+		try (CloseableHttpAsyncClient client = HttpAsyncClients.custom().setIOReactorConfig(ioReactorConfig).build()) {
+			client.start();
+
+			JSONObject content = new JSONObject().put("prompt", prompt).put("n", n);
+			var requestBuilder = AsyncRequestBuilder.post();
+			requestBuilder.setHttpHost(new HttpHost(API_STRING_HOST));
+			requestBuilder.setScheme("https");
+			requestBuilder.setPath(API_STRING_URI);
+			requestBuilder.setEntity(content.toString());
+			requestBuilder.addHeader("Content-Type", "application/json");
+			requestBuilder.addHeader("Authorization", "Bearer " + API_KEY);
+			System.out.println(requestBuilder.toString());
+
+			AsyncRequestProducer requestProducer = requestBuilder.build();
+			AsyncResponseConsumer<SimpleHttpResponse> requestConsumer = SimpleResponseConsumer.create();
+			FutureCallback<SimpleHttpResponse> callback = new ImageGeneratorRequestCallback(targetFolderPath);
+			var future = client.execute(requestProducer, requestConsumer, callback);
+			future.get();
+		}
+	}
+
+	private void parseResponse(SimpleHttpResponse result, Path targetFolderPath, String prefix) throws IOException {
+		System.out.println(result.toString());
+		System.out.println(result.getBodyText());
+		JSONObject json = new JSONObject(result.getBodyText());
+		JSONArray data = json.getJSONArray("data");
+		for (int i = 0; i < data.length(); i++) {
+			JSONObject object = data.getJSONObject(i);
+			String urlString = object.getString("url");
+			URL url = new URL(urlString);
+			Path imagePath = targetFolderPath.resolve(prefix + "_" + Integer.toString(i + 1) + ".png");
+			downloadImage(url, imagePath);
+		}
+	}
+
+	private void downloadImage(URL url, Path targetPath) throws IOException {
+		FileUtils.copyURLToFile(url, targetPath.toFile());
+	}
+
+	private class ImageGeneratorRequestCallback implements FutureCallback<SimpleHttpResponse> {
+		private String prefix = Long.toString(Instant.now().getEpochSecond());
+		private Path targetFolderPath;
+
+		ImageGeneratorRequestCallback(Path targetFolderPath) {
+			this.targetFolderPath = targetFolderPath;
+		}
+
+		@Override
+		public void completed(SimpleHttpResponse result) {
+			System.err.println("Good request");
+			try {
+				parseResponse(result, targetFolderPath, prefix);
+			} catch (Exception e) {
+				System.err.println("Failed parse");
+				e.printStackTrace();
+			}
+		}
+
+		@Override
+		public void failed(Exception ex) {
+			System.err.println("Failed request");
+			ex.printStackTrace();
+
+		}
+
+		@Override
+		public void cancelled() {
+			System.err.println("Failed cancelled");
+		}
+
+	}
+
+	public static class ImageGeneratorException extends Exception {
+		private static final long serialVersionUID = -369288247714369519L;
+
+		private ImageGeneratorException(String message, Throwable cause) {
+			super(message, cause);
+		}
+
+	}
+}
